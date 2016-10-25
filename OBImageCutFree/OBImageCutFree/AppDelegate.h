1f629823820f7b328f56e69f24e4f90736ec401c
王博 <15711361606@163.com>
2016-10-25 12:09:49 +0800

    初始化项目，添加pod

---
+++ OBImageCutFree/OBImageCutFree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OBImageCutFree
+//
+//  Created by oneBool on 2016/10/25.
+//  Copyright © 2016年 oneBool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBImageCutFree/OBImageCutFree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OBImageCutFree
+//
+//  Created by oneBool on 2016/10/25.
+//  Copyright © 2016年 oneBool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
